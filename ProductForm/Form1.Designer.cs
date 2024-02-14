c15740d0837da5f098e62fb14baeed8042445202
sk3l3tonis <kostik63737@gmail.com>
2024-02-14 21:26:16 +0300

    Добавьте файлы проекта.

---
+++ ProductForm/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ProductForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.chtPriceDistribution = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnGetProducts = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chtPriceDistribution)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Location = new System.Drawing.Point(12, 12);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.RowHeadersWidth = 51;
+            this.dgvProducts.RowTemplate.Height = 24;
+            this.dgvProducts.Size = new System.Drawing.Size(470, 360);
+            this.dgvProducts.TabIndex = 0;
+            // 
+            // chtPriceDistribution
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chtPriceDistribution.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chtPriceDistribution.Legends.Add(legend2);
+            this.chtPriceDistribution.Location = new System.Drawing.Point(488, 12);
+            this.chtPriceDistribution.Name = "chtPriceDistribution";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chtPriceDistribution.Series.Add(series2);
+            this.chtPriceDistribution.Size = new System.Drawing.Size(415, 360);
+            this.chtPriceDistribution.TabIndex = 1;
+            this.chtPriceDistribution.Text = "chart1";
+            // 
+            // btnGetProducts
+            // 
+            this.btnGetProducts.Location = new System.Drawing.Point(189, 379);
+            this.btnGetProducts.Name = "btnGetProducts";
+            this.btnGetProducts.Size = new System.Drawing.Size(119, 65);
+            this.btnGetProducts.TabIndex = 2;
+            this.btnGetProducts.Text = "Вывод списка продуктов";
+            this.btnGetProducts.UseVisualStyleBackColor = true;
+            this.btnGetProducts.Click += new System.EventHandler(this.btnGetProducts_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 450);
+            this.Controls.Add(this.btnGetProducts);
+            this.Controls.Add(this.chtPriceDistribution);
+            this.Controls.Add(this.dgvProducts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chtPriceDistribution)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvProducts;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chtPriceDistribution;
+        private System.Windows.Forms.Button btnGetProducts;
+    }
+}
+
---
+++ ProductForm/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using Npgsql;
+
+namespace ProductForm
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Host=localhost; Port=5432; Database=productform; Username=postgres; Password=1212321";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetProducts_Click(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT name, price, article FROM prodform";
+                using (NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(query, connection))
+                {
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds, "prodform");
+                    dgvProducts.DataSource = ds.Tables["prodform"];
+                }
+            }
+
+            DrawPriceDistribution();
+        }
+
+        private void DrawPriceDistribution()
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT price FROM prodform";
+                using (NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(query, connection))
+                {
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    Dictionary<string, int> priceCategories = new Dictionary<string, int>();
+                    priceCategories["Категория 1 (от 40 до 300 рублей)"] = dt.AsEnumerable().Count(row => row.Field<decimal>("price") >= 40 && row.Field<decimal>("price") <= 300);
+                    priceCategories["Категория 2 (от 350 до 750 рублей)"] = dt.AsEnumerable().Count(row => row.Field<decimal>("price") >= 350 && row.Field<decimal>("price") <= 750);
+                    priceCategories["Категория 3 (от 1000 до 1700 рублей)"] = dt.AsEnumerable().Count(row => row.Field<decimal>("price") >= 1000 && row.Field<decimal>("price") <= 1700);
+
+                    chtPriceDistribution.Series.Clear();
+                    chtPriceDistribution.Titles.Clear();
+                    chtPriceDistribution.Titles.Add("Распределение товаров по ценовым категориям");
+
+                    Series series = new Series();
+                    series.Name = "Ценовые категории";
+                    series.ChartType = SeriesChartType.Column;
+
+                    foreach (KeyValuePair<string, int> category in priceCategories)
+                    {
+                        series.Points.AddXY(category.Key, category.Value);
+                    }
+
+                    chtPriceDistribution.Series.Add(series);
+                    chtPriceDistribution.ChartAreas[0].AxisX.Interval = 1;
+                }
+            }
+        }
+
+    }
+}
+        
+    
